861e446d82541d2fe918d53f74ec61aef84d84aa
oleoot <oleoot28@gmail.com>
2020-05-22 17:16:38 +0300

    Sending tweets to mongodb database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const monk = require('monk');
 
 
 const app = express();
+
+
+const db = monk('localhost/database');
+const tweets = db.get('tweets')
+
+
+
+
 
 app.use(cors())
 app.use(express.json())
@@ -22,10 +31,14 @@ app.post('/tweets', (req, res) => {
     if (isValidTweet(req.body)) {
         const tweet = {
             name: req.body.name.toString(),
-            content: req.body.content.toString()
-
+            content: req.body.content.toString(),
+            created: new Date()
         }
-        console.log(tweet)
+        tweet
+            .insert(tweet)
+            .then(createdTweet => {
+                res.json(createdTweet)
+            });
     } else {
         res.status(422);
         res.json({
